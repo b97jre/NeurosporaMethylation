35945ffaec1754a676eea21c311029809d0cdd1f
jorei499 <johan.reimegard@scilifelab.se>
2023-10-12 11:04:03 +0200

    Updating with new rmd files and sbatch files

---
+++ scripts/colorSchemes.R
@@ -0,0 +1,28 @@
+
+
+tissueLevels = c("veg","sex")
+strainLeveles = c("L1","L6","L10")
+mixLevels =   c("a","R1","R2","R3","A")
+typeLevels = c("homokaryon","heterokaryon")
+
+
+mixColors = c(a = "#440154FF",
+                 R1 = "#31688EFF",
+                R2 = "#1F9E89FF",
+                R3 ="#6DCD59FF",
+                A = "#FDE725FF")
+
+
+strainColors  = c(L1 ="#4CED72",
+                L6 ="#76A6F4",
+                L10 ="#EDA524")
+
+tissueColors = c(
+  veg = "#167b2b",
+  sex = "#fbcab9")
+
+typeColor = c(
+  heterokaryon = "#9E4D0B",
+  homokaryon = "#7FD6EA")
+
+
